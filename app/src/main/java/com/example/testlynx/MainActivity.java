f55632e7a34b633c81bdbfa9c92668e5c65ac8bc
Hayk232002 <hayknazaryan10@gmail.com>
2019-06-21 02:48:06 +0400

    Int

--- app/src/main/java/com/example/testlynx/MainActivity.java
+++ app/src/main/java/com/example/testlynx/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    int zhdgsj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
