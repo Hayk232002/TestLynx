651766c6efc7f7ecc8d32a8518ceae7f6a96896e
Hayk232002 <hayknazaryan10@gmail.com>
2019-06-21 02:29:43 +0400

    Btn

--- app/src/main/java/com/example/testlynx/MainActivity.java
+++ app/src/main/java/com/example/testlynx/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    Button bsvdvbhjn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
